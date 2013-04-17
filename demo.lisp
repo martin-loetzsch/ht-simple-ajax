42a09cf03aaae5268c89e72bc4ca46d11885d495
Martin Loetzsch <loetzsch@krks.local>
2013-04-17 23:19:48 +0200

    moved from private svn to github

--- demo.lisp
+++ demo.lisp
@@ -1,4 +1,4 @@
-;;;;; Copyright (c) 2010-2012, Martin Loetzsch
+;;;;; Copyright (c) 2010-2013, Martin Loetzsch
 ;;;;; All rights reserved.
 
 ;;;;; Redistribution and use in source and binary forms, with or
